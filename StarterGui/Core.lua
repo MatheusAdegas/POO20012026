a1b48e85340202f10ac9082bbeb2ab1578d3fed5
MatheusAdegas <matheusnascimentoadegas@alu.ufc.br>
2026-01-20 18:38:29 -0300

    Comentario sobre SetCoreGuiEnabled

--- StarterGui/Core.lua
+++ StarterGui/Core.lua
@@ -9,10 +9,13 @@ local UIPlantController = require(
 local screenGui = script.Parent :: ScreenGui
 local main = screenGui:WaitForChild("Main") :: Frame
 
-UIPlantController.Start(main)
+UIPlantController.Start(main) -- inicialização do sistema de conexão da GUI
 
 -- disable coregui
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)
+
+-- ^ desativando alguns recursos nativos de GUI que n~~ao são nnecessários
